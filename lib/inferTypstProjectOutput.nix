f42ea9244d81cee64c8723ef819c7b8d7731279e
loqusion <38332081+loqusion@users.noreply.github.com>
2023-12-26 15:03:02 -0600

    feat(watchTypstProject): implementation

---
+++ lib/inferTypstProjectOutput.nix
@@ -0,0 +1,14 @@
+{lib}: args: let
+  supportedFormats = ["pdf" "svg" "png"];
+  format = ({format = "pdf";} // args.typstOpts or {}).format;
+  name = lib.strings.removeSuffix ".typ" (builtins.baseNameOf args.typstProjectSource);
+  extension =
+    if builtins.elem format supportedFormats
+    then format
+    else
+      lib.flip lib.trivial.warn "" ''
+        typst.nix could not infer the typst output extension since ${format} is not supported
+        to silence this warning, set the format option to one of: pdf, svg, png
+      '';
+in
+  name + lib.optionalString (extension != "") ".${extension}"
--- lib/watchTypstProject.nix
+++ lib/watchTypstProject.nix
@@ -1,1 +1,56 @@
-{mkTypstDerivation}: {}: mkTypstDerivation {}
+{
+  inferTypstProjectOutput,
+  lib,
+  linkLocalPaths,
+  pkgs,
+  typst,
+  typstOptsFromArgs,
+}: args @ {
+  fontPaths ? [],
+  forceLocalPaths ? false,
+  localPaths ? [],
+  typstProjectSource ? "main.typ",
+  typstWatchCommand ? "typst watch",
+  ...
+}: let
+  inherit (builtins) removeAttrs;
+  inherit (lib) optionalString;
+  inherit (lib.strings) concatStringsSep escapeShellArg;
+
+  typstOpts = typstOptsFromArgs (args.typstOpts or {});
+  typstProjectOutput =
+    args.typstProjectOutput
+    or (inferTypstProjectOutput (
+      {inherit typstProjectSource;} // args
+    ));
+
+  cleanedArgs = removeAttrs args [
+    "fontPaths"
+    "forceLocalPaths"
+    "localPaths"
+    "text"
+    "typstProjectOutput"
+    "typstProjectSource"
+    "typstWatchCommand"
+  ];
+in
+  pkgs.writeShellApplication (cleanedArgs
+    // {
+      name = args.name or "typst-watch";
+
+      runtimeInputs = [
+        typst
+      ];
+
+      text =
+        optionalString (fontPaths != []) ''
+          export TYPST_FONT_PATHS=${concatStringsSep ":" fontPaths}
+        ''
+        + optionalString (localPaths != []) (linkLocalPaths {
+          inherit localPaths forceLocalPaths;
+        })
+        + ''
+
+          ${typstWatchCommand} ${typstOpts} ${escapeShellArg typstProjectSource} ${escapeShellArg typstProjectOutput}
+        '';
+    })
