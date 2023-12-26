f8bd80bcbcf35df6d8100686838a057d008195b9
loqusion <38332081+loqusion@users.noreply.github.com>
2023-12-26 15:28:23 -0600

    chore: update warning text

--- lib/inferTypstProjectOutput.nix
+++ lib/inferTypstProjectOutput.nix
@@ -8,7 +8,9 @@
     else
       lib.flip lib.trivial.warn "" ''
         typst.nix could not infer the typst output extension since ${format} is not supported
-        to silence this warning, set the format option to one of: pdf, svg, png
+        to silence this warning consider one of the following:
+        - set the format option to one of: pdf, svg, png
+        - set `typstProjectOutput` explicitly
       '';
 in
   name + lib.optionalString (extension != "") ".${extension}"
