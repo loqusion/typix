60d5554b00b70d9c5fd0b0e111eee0bd460de9ef
loqusion <38332081+loqusion@users.noreply.github.com>
2023-12-26 09:52:32 -0600

    feat(devShell): handle `fontPaths` and `localPaths`

--- lib/devShell.nix
+++ lib/devShell.nix
@@ -1,30 +1,38 @@
 {
   lib,
+  linkLocalPathsHook,
   mkShell,
   typst,
 }: args @ {
   checks ? {},
   extraShellHook ? "",
+  fontPaths ? [],
   inputsFrom ? [],
   localPaths ? [],
   packages ? [],
   ...
 }: let
   inherit (builtins) removeAttrs;
+  inherit (lib) optionalAttrs;
+  inherit (lib.strings) concatStringsSep;
 
-  # optionalLinkLocalPathsHook =
-  #   if localPaths != []
-  #   then linkLocalPathsHook localPaths
-  #   else "";
+  optionalLinkLocalPathsHook =
+    if localPaths != []
+    then linkLocalPathsHook localPaths
+    else "";
 
   cleanedArgs = removeAttrs args [
     "checks"
     "extraShellHook"
+    "fontPaths"
     "inputsFrom"
     "localPaths"
   ];
 in
   mkShell (cleanedArgs
+    // optionalAttrs (fontPaths != []) {
+      TYPST_FONT_PATHS = concatStringsSep ":" fontPaths;
+    }
     // {
       inputsFrom = builtins.attrValues checks ++ inputsFrom;
 
@@ -37,6 +45,7 @@ in
       shellHook =
         args.shellHook
         or ''
+          ${optionalLinkLocalPathsHook}
           ${extraShellHook}
         '';
     })
