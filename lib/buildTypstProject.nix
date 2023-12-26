4e4b01c915166129aa48b974b060c78e0f6041f1
loqusion <38332081+loqusion@users.noreply.github.com>
2023-12-26 05:32:52 -0600

    feat(buildTypstProject): remove `typstProjectOutput`

--- lib/buildTypstProject.nix
+++ lib/buildTypstProject.nix
@@ -9,22 +9,9 @@
   typstProjectSource,
   ...
 }: let
-  inherit (builtins) isPath removeAttrs typeOf;
-  inherit (lib.asserts) assertMsg;
+  inherit (builtins) removeAttrs;
   inherit (lib.strings) escapeShellArg;
 
-  # TODO: Experiment with exposing this to the user. If it doesn't write to the user's directory,
-  # then just write to $out and use this attribute in a `pkgs.writeShellScriptBin` derivation
-  # with something like:
-  #     typstNixOutput=$(nix build .#default --no-link --print-out-paths)
-  #     cp -L --no-preserve=mode "$typstNixOutput" ./${escapeShellArg typstProjectOutput}
-  typstProjectOutput =
-    if args ? typstProjectOutput
-    then
-      assert assertMsg
-      (isPath args.typstProjectOutput)
-      "typstProjectOutput must be a path; received ${typeOf args.typstProjectOutput}"; (escapeShellArg args.typstProjectOutput)
-    else "$out";
   typstOpts = typstOptsFromArgs (args.typstOpts or {});
   cleanedArgs = removeAttrs args [
     "localPaths"
@@ -35,7 +22,7 @@ in
   mkTypstDerivation (cleanedArgs
     // {
       buildPhaseTypstCommand = ''
-        ${typstCompileCommand} ${typstOpts} ${escapeShellArg typstProjectSource} ${typstProjectOutput}
+        ${typstCompileCommand} ${typstOpts} ${escapeShellArg typstProjectSource} "$out"
       '';
 
       nativeBuildInputs = [
