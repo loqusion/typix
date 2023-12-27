413c25671eee86db26c6bebbf2e07da16d6d879b
loqusion <38332081+loqusion@users.noreply.github.com>
2023-12-26 18:04:15 -0600

    feat(buildLocalTypstProject): initial implementation

---
+++ lib/buildLocalTypstProject.nix
@@ -0,0 +1,29 @@
+{
+  buildTypstProject,
+  inferTypstProjectOutput,
+  lib,
+  pkgs,
+  typst,
+}: args @ {typstProjectSource ? "main.typ", ...}: let
+  inherit (lib.strings) escapeShellArg;
+
+  typstProjectOutput =
+    args.typstProjectOutput
+    or (
+      inferTypstProjectOutput (
+        {inherit typstProjectSource;} // args
+      )
+    );
+in
+  pkgs.writeShellApplication {
+    name = "typst-build-local";
+
+    runtimeInputs = [
+      typst
+    ];
+
+    text = ''
+      nix_out_path=$(nix build ${buildTypstProject args} --print-out-paths)
+      cp -L --no-preserve=mode "$nix_out_path" ${escapeShellArg typstProjectOutput}
+    '';
+  }
--- lib/default.nix
+++ lib/default.nix
@@ -5,6 +5,7 @@
 lib.makeScope newScope (self: let
   inherit (self) callPackage;
 in {
+  buildLocalTypstProject = callPackage ./buildLocalTypstProject.nix {};
   buildTypstProject = callPackage ./buildTypstProject.nix {};
   coerceLocalPathAttr = callPackage ./coerceLocalPathAttr.nix {};
   copyLocalPathsHook = callPackage ./setupHooks/copyLocalPaths.nix {};
