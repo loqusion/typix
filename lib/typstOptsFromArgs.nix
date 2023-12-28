f9cece8d7fc19ef5bfccf621860c23ada2e35e33
loqusion <38332081+loqusion@users.noreply.github.com>
2023-12-27 22:06:33 -0600

    feat(typstOptsFromArgs): infer `format` from `typstProjectOutput` extension

--- lib/typstOptsFromArgs.nix
+++ lib/typstOptsFromArgs.nix
@@ -1,16 +1,42 @@
-{lib}: args_: let
+{lib}: origArgs: let
+  inherit (builtins) elem isBool tail;
+  inherit (lib) optionalAttrs optionalString;
   inherit (lib.attrsets) filterAttrs mapAttrsToList recursiveUpdate;
-  inherit (lib.strings) concatStringsSep escapeShellArg;
+  inherit (lib.lists) last;
+  inherit (lib.strings) concatStringsSep escapeShellArg splitString;
+
+  inferredFormatFromTypstProjectOutput = optionalString (! origArgs ? typstOpts.format) (let
+    supportedExtensions = ["pdf" "svg" "png"];
+    typstProjectOutputExt = optionalString (origArgs ? typstProjectOutput) (let
+      splitTypstProjectOutput = splitString "." origArgs.typstProjectOutput;
+    in
+      optionalString (tail splitTypstProjectOutput != []) (
+        last splitTypstProjectOutput
+      ));
+  in
+    optionalString (elem typstProjectOutputExt supportedExtensions)
+    typstProjectOutputExt);
 
   defaultArgs = {
     typstOpts = {
       format = "pdf";
     };
   };
-  args = recursiveUpdate defaultArgs args_;
+  args =
+    recursiveUpdate (
+      recursiveUpdate defaultArgs (optionalAttrs
+        (inferredFormatFromTypstProjectOutput != "") {
+          typstOpts = {
+            format = inferredFormatFromTypstProjectOutput;
+          };
+        })
+    )
+    origArgs;
 in
   concatStringsSep " " (
     mapAttrsToList
-    (opt: value: "--${opt} ${escapeShellArg value}")
-    (filterAttrs (_: v: !isNull v) args.typstOpts)
+    (opt: value:
+      "--${opt}"
+      + optionalString (!isBool value) " ${escapeShellArg value}")
+    (filterAttrs (_: v: v != false && !isNull v) args.typstOpts)
   )
