e1abd8b1ba9a3a15c54ed92a4a39777a31f972b7
loqusion <38332081+loqusion@users.noreply.github.com>
2024-01-04 11:45:08 -0600

    feat(cleanTypstSource): allow typst.toml just in case

--- lib/cleanTypstSource.nix
+++ lib/cleanTypstSource.nix
@@ -1,5 +1,11 @@
 {lib}: src:
 lib.cleanSourceWith {
   src = lib.cleanSource src;
-  filter = path: type: type == "directory" || (lib.hasSuffix ".typ" path);
+  filter = path: type: let
+    isTypstSource = lib.hasSuffix ".typ" path;
+    isSpecialFile = builtins.elem path [
+      "typst.toml"
+    ];
+  in
+    type == "directory" || isTypstSource || isSpecialFile;
 }
