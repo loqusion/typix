f9148a13f592a2c5691c51b072c1039e175f9b45
loqusion <38332081+loqusion@users.noreply.github.com>
2024-11-19 09:19:07 -0600

    docs: add example showing how to use typst packages
    Fixes #32

---
+++ examples/typst-packages/flake.nix
@@ -0,0 +1,119 @@
+{
+  description = "A Typst project that uses Typst packages";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    typix = {
+      url = "github:loqusion/typix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    typst-packages = {
+      url = "github:typst/packages";
+      flake = false;
+    };
+
+    # Example of downloading icons from a non-flake source
+    # font-awesome = {
+    #   url = "github:FortAwesome/Font-Awesome";
+    #   flake = false;
+    # };
+  };
+
+  outputs = inputs @ {
+    nixpkgs,
+    typix,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (pkgs) lib;
+
+      typixLib = typix.lib.${system};
+
+      src = typixLib.cleanTypstSource ./.;
+      commonArgs = {
+        typstSource = "main.typ";
+
+        fontPaths = [
+          # Add paths to fonts here
+          # "${pkgs.roboto}/share/fonts/truetype"
+        ];
+
+        virtualPaths = [
+          # Add paths that must be locally accessible to typst here
+          # {
+          #   dest = "icons";
+          #   src = "${inputs.font-awesome}/svgs/regular";
+          # }
+        ];
+      };
+      typstPackagesSrc = pkgs.symlinkJoin {
+        name = "typst-packages-src";
+        paths = [
+          "${inputs.typst-packages}/packages"
+          # More Typst packages can be added here
+        ];
+      };
+      typstPackagesCache = pkgs.stdenv.mkDerivation {
+        name = "typst-packages-cache";
+        src = typstPackagesSrc;
+        dontBuild = true;
+        installPhase = ''
+          mkdir -p "$out/typst/packages"
+          cp -LR --reflink=auto --no-preserve=mode -t "$out/typst/packages" "$src"/*
+        '';
+      };
+
+      # Compile a Typst project, *without* copying the result
+      # to the current directory
+      build-drv = typixLib.buildTypstProject (commonArgs
+        // {
+          inherit src;
+          XDG_CACHE_HOME = typstPackagesCache;
+        });
+
+      # Compile a Typst project, and then copy the result
+      # to the current directory
+      build-script = typixLib.buildTypstProjectLocal (commonArgs
+        // {
+          inherit src;
+          XDG_CACHE_HOME = typstPackagesCache;
+        });
+
+      # Watch a project and recompile on changes
+      watch-script = typixLib.watchTypstProject commonArgs;
+    in {
+      checks = {
+        inherit build-drv build-script watch-script;
+      };
+
+      packages.default = build-drv;
+
+      apps = rec {
+        default = watch;
+        build = flake-utils.lib.mkApp {
+          drv = build-script;
+        };
+        watch = flake-utils.lib.mkApp {
+          drv = watch-script;
+        };
+      };
+
+      devShells.default = typixLib.devShell {
+        inherit (commonArgs) fontPaths virtualPaths;
+        packages = [
+          # WARNING: Don't run `typst-build` directly, instead use `nix run .#build`
+          # See https://github.com/loqusion/typix/issues/2
+          # build-script
+          watch-script
+          # More packages can be added here, like typstfmt
+          # pkgs.typstfmt
+        ];
+      };
+    });
+}
---
+++ examples/typst-packages/main.typ
@@ -0,0 +1,4 @@
+#import "@preview/example:0.1.0"
+
+= Lorem ipsum
+#lorem(30)
